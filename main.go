247a9ff266a65ba98cce7fea4172506190e171c7
webasif <asifcse00@@gmail.com>
2021-02-01 11:38:02 +0600

    new codes

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
-
 import "fmt"
 
-func main ()	 {
-
- fmt.Println ("Hello Dhaka")
- fmt.Println("github local to remote")
+func main(){
+fmt.Println("Hello there")
+var name, city string = "Asif", "Dhaka"
+fmt.Println(name, city)
+var c rune = 'A'
+var age1 int = 20
+var result float32 = 100.02
+fmt.Println(c, age1, result)
 }
